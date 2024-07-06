9a47eed28fd4bb7fb2accf8e5d4a6eac8acce674
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2024-07-06 23:25:20 +0300

    added design for profile picture change

--- src/components/profile/profile-picture-inaccount.tsx
+++ src/components/profile/profile-picture-inaccount.tsx
@@ -1,13 +1,17 @@
 import { useUser } from "@/lib/hooks/useUser";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 
 export function InaccountProfilePicture() {
   const { profilePictureUrl } = useUser();
 
+  const handleProfilePictureChange = () => {};
+
   return (
     <div className="w-full -z-10 flex py-2 flex-col justify-center items-center absolute left-0 top-0 -translate-y-1/2">
-      <div className="w-fit aspect-square max-w-52">
-        <Avatar className="p-0 m-0">
+      <div className="w-fit aspect-square max-w-52 rounded-full relative ">
+        <Avatar className="p-0 m-0 ">
           <AvatarImage
             src={profilePictureUrl}
             className="rounded-full aspect-square w-full gradient-mask-t-[rgba(0,0,0,0)_0,rgba(0,0,0,0.9)_50%,rgba(0,0,0,1)_100%]"
@@ -19,6 +23,19 @@ export function InaccountProfilePicture() {
             />
           </AvatarFallback>
         </Avatar>
+        <div
+          onClick={() => handleProfilePictureChange()}
+          className="absolute group top-0 left-0 w-full flex justify-center gap-1 flex-col items-center cursor-pointer transition-all hover:backdrop-blur-sm hover:bg-black/35 rounded-full h-full"
+        >
+          <FontAwesomeIcon
+            icon={faCamera}
+            className="text-white transition-all opacity-0 group-hover:opacity-100"
+            size="3x"
+          />
+          <p className="text-white text-xs font-semibold opacity-0 group-hover:opacity-100 transition-all">
+            Смени снимка
+          </p>
+        </div>
       </div>
     </div>
   );
